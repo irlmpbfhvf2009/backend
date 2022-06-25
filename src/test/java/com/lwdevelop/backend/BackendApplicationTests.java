240baa9f883bd485370b42b30a3fb7bf1bd5fd23
HACKER <hyprocrite@kimo.com>
2022-06-25 23:58:12 +0800

    master

--- src/test/java/com/lwdevelop/backend/BackendApplicationTests.java
+++ src/test/java/com/lwdevelop/backend/BackendApplicationTests.java
@@ -1,16 +1,9 @@
 package com.lwdevelop.backend;
 
-import java.sql.SQLException;
-import java.util.Optional;
-
-import javax.sql.DataSource;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.lwdevelop.backend.Entity.Member;
 import com.lwdevelop.backend.Repository.MemberRepository;
 
 import org.junit.runner.RunWith;
@@ -22,14 +15,6 @@ class BackendApplicationTests {
     @Autowired
     MemberRepository memberRepository;
 
-	@Test
-    public void test1() throws SQLException {
-		Member member = new Member();
-		member.setEmail("email");
-		member.setPassword("password");
-		member.setName("name");
-		memberRepository.save(member);
-    }
 	@Test
 	void contextLoads() {
 	}
