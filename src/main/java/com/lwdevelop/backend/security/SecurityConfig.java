695e657e3e789d929c6919e6df14f3fe14a25eae
irlmpbfhvf2009 <hyprocrite1631@gmail.com>
2022-06-27 17:38:00 +0800

    master

---
+++ src/main/java/com/lwdevelop/backend/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.lwdevelop.backend.security;
+
+
+/*
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+ */
+
+/**
+ * SpringSecurity 5.4.x以上新用法配置
+ * 為避免循環依賴，僅用於配置HttpSecurity
+ * Created by leo on 2022/6/27.
+ */
+
+ /*
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        //省略HttpSecurity的配置
+        return httpSecurity
+                    
+                    .build();
+    }
+    
+}
+ */
