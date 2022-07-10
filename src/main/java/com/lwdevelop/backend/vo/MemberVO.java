ab04bdc45019272beda7fb9fd906e1a620497141
HACKER <hyprocrite@kimo.com>
2022-07-11 03:48:41 +0800

    實體類增加詳細資料

--- src/main/java/com/lwdevelop/backend/vo/MemberVO.java
+++ src/main/java/com/lwdevelop/backend/vo/MemberVO.java
@@ -10,13 +10,13 @@ import lombok.ToString;
 @ToString
 public class MemberVO {
     @ApiModelProperty(value = "編號", required = true)
-	private Integer id;
+    private Integer id;
     @ApiModelProperty(value = "用戶名", required = true)
-	private String username;
+    private String username;
     @ApiModelProperty(value = "信箱", required = true)
-	private String email;
+    private String email;
     @ApiModelProperty(value = "密碼", required = true)
-	private String password;
+    private String password;
     @ApiModelProperty(value = "性別", required = true)
     private String gender;
     @ApiModelProperty(value = "年齡", required = true)
@@ -25,6 +25,30 @@ public class MemberVO {
     private String constellation;
     @ApiModelProperty(value = "令牌", required = true)
     private String token;
-    @ApiModelProperty(value = "登入信箱",required = true)
+    @ApiModelProperty(value = "登入信箱", required = true)
     private String loginEmail;
+    
+    @ApiModelProperty(value = "身高", required = true)
+    private Integer height;
+    @ApiModelProperty(value = "體重", required = true)
+    private Integer weight;
+    @ApiModelProperty(value = "體型", required = true)
+    private String bodyType;
+    @ApiModelProperty(value = "居住地區", required = true)
+    private String liveArea; 
+    @ApiModelProperty(value = "自我介紹", required = true)
+    private String selfIntroduction; 
+    @ApiModelProperty(value = "理想型", required = true)
+    private String myType; 
+    @ApiModelProperty(value = "職業類別", required = true)
+    private String profession; 
+    @ApiModelProperty(value = "學歷", required = true)
+    private String Education;
+    @ApiModelProperty(value = "語言", required = true)
+    private String language;
+    @ApiModelProperty(value = "是否有抽菸", required = true)
+    private String smokes;
+    @ApiModelProperty(value = "是否有飲酒習慣", required = true)
+    private String drinking;
+
 }
